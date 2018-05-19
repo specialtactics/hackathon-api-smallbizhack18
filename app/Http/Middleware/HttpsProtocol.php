2131eb60f0cd2bdc7b8ca2e60d8cda3887de3bae
Christopher Turner <turner296@gmail.com>
2018-05-19 12:48:06 +1000

    trusting proxies

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -9,13 +9,6 @@ class HttpsProtocol
 {
     public function handle(Request $request, Closure $next)
     {
-        dump($request->isFromTrustedProxy());
-
-        dump($request->secure());
-        dump(env('APP_ENV'));
-
-        die;
-
         if (!$request->secure() && env('APP_ENV') === 'production') {
             return redirect()->secure($request->getRequestUri());
         }
