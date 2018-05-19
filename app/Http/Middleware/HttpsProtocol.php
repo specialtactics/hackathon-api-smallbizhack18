8cbced7ff02f10027b5118ec0945d81c1a54c921
Christopher Turner <turner296@gmail.com>
2018-05-19 12:41:55 +1000

    more

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -10,9 +10,6 @@ class HttpsProtocol
     public function handle(Request $request, Closure $next)
     {
         dump($request->isFromTrustedProxy());
-        dump($proto = $request->getTrustedValues(Request::HEADER_X_FORWARDED_PROTO));
-
-        dump(in_array(strtolower($proto[0]), array('https', 'on', 'ssl', '1'), true));
 
         dump($request->secure());
         dump(env('APP_ENV'));
